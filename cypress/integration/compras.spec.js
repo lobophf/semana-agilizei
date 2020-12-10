81eb739596e92c9609cbf6de3006813b4792c0f2
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-12-10 00:02:01 -0300

    add a new lecture

--- cypress/integration/compras.spec.js
+++ cypress/integration/compras.spec.js
@@ -1,20 +1,33 @@
-context('',() => {
+context('Compra', () => {
 	it('Efetuar uma compra de produto', () => {
+		cy.backgroundLogin();
 		cy.visit('/');	
 		let nomeProduto = 'Faded Short Sleeve T-shirts';
 		cy.contains(nomeProduto).trigger('mouseover');
 		cy.contains(nomeProduto).parent().siblings('div.button-container').children('a').first().click()
+		cy.get(".icon-ok").parent().should('contain.text','Product successfully added to your shopping cart');
+		cy.get('span#layer_cart_product_title').should('contain.text', nomeProduto);
+
 		cy.get(".button-container a[href$='controller=order']").click();
 		cy.get(".cart_navigation a[href$='controller=order&step=1']").click();
-		cy.get('#email').type('semana-agilizei@mail.com');
-		cy.get('#passwd').type('12345');
-		cy.get('button#SubmitLogin').click();
+//		cy.get('#email').type('semana-agilizei@mail.com');
+//		cy.get('#passwd').type('12345');
+//		cy.get('button#SubmitLogin').click();
+
+		cy.get('[type=checkbox]#addressesAreEquals').should('have.attr', 'checked', 'checked');
+
 		cy.get('button[name=processAddress]').click();
 		cy.get('[type=checkbox]#cgv').check();
 		cy.get('button[name=processCarrier]').click();
 		cy.get('.bankwire').click();
 		cy.get('.cart_navigation button[type=submit]').find('span').contains('I confirm my order').click();
 		cy.get('.cheque-indent strong').should('contain.text', 'Your order on My Store is complete.')
+		cy.get('div.box').invoke('text').then((text) => {
+			cy.writeFile('cypress/fixtures/pedido.json', {id: `${text.match(/[A-Z][A-Z]+/g)[1]}`});
+		})
+		cy.get(".cart_navigation a[href$='history']").click();
+		cy.readFile('cypress/fixtures/pedido.json').then((pedido) => {
+			cy.get('tr.first_item .history_link a').should('contain.text', pedido.id);
+		})
 	});
-
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('backgroundLogin', () => {
+	cy.setCookie(
+		'PrestaShop-a30a9934ef476d11b6cc3c983616e364',
+			'R6xmma6F4U6edNQuu67M0sF74EIOeYh%2F3UqLR6z0ZYqEPqQ%2FL%2F6rzkjf47EztOVsPcuxhUoa3S3U2g1Rza7TQkfyMSnPfhsRtHFtkThBLpjPLobEOxnIooNKcMNsxHUuDJg2%2FE%2FN29Ipz9gLVEV9pEsPZ1hN4iWDgzXnlan0YvtAAtNsAG2gMGRuyWpadidpxbvs2WHkyAzqj5nRHBU03PBF1i9x9mffpqgwdlUDzqGOdzq4ugPZBPfpupnFUTovYIYZQ1uHNoJP9l15maoXapG6qrGlH3xN0yToXCyB%2FNjo2PRQHjzIc%2FYVfrZ%2FdzZVeWfIpccwBxKYMS%2BT6HK1mNwNZIqEknFzq6%2B%2Fb7ek4rIkAzhtFTTBCncc6KlKcl1HTIyNsafztQ%2BZkpyhJenfaQIpA7COVM%2BjRJBveF%2BdPV0dbGzAvhcphqUbHGtfBkv7IdeVVmLFNiQHr4qbfpi96sqBAKpEnKbWg%2FMIB14XPMM%3D000355'
+	)
+});
